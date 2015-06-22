675b56e36d93213fc8e2588112ee65924c88dc25
te444 <senior.te444@yandex.ru>
2015-06-22 23:41:47 +0300

    1

---
+++ indexsdf.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       <meta http-equiv=Content-Type content="text/html;charset=UTF-8">
+       <title>Металлоискатель</title>
+       <link href="css/style.css" rel="stylesheet" />
+       <link href="css/jquery.bxslider.css" rel="stylesheet" />
+    </head>    
+    <body>
+        
+        
+        <div id="header">
+            <div id="navigation">
+                <div id="left_nav">
+                <ul>
+                    <li>О нас</li>
+                    <li>R 100</li>
+                    <li>R 200</li>
+                    <li>R 300</li>
+                </ul>  
+                </div>
+                <div id="right_nav">
+                <ul>
+                    <li>Сделать заказ</li>
+                    <li>Доставка</li>
+                </ul>  
+                </div> 
+            </div>
+        </div>
+        <div id="about_us">
+            <div id="prev_about">
+            <p>Производственно-промышленная марка "Dart" является совместным производством России и Германии.</p>
+            <p> Мы небольшая команда которая уже более 10 лет связана обслуживанием и продажей с металлоискателей. </p>
+                <p> Сплоченность коллектива и знание своих обязанностей позволяет каждому покупателю получить свою покупку в максимально короткие сроки с приятными бонусами.
+               <p>  С особой бережностью мы упаковываем товар,благодаря чему наши клиенты лишний раз благодарят нас за качественное выполнение заказа.</p>
+               
+               <p>
+                   <ol>
+                    <li>Простота оформление заказа.</li>
+                    <li>Быстрая доставка в ваш регион товара.</li>
+                    <li>Тех поддержка в любое время дня и суток.</li>
+                   </ol>
+               </p>
+        </div>
+            <div id="">
+            <ul class="bxslider">
+  <li><img src="aaa.jpg" /></li>
+  <li><img src="eee.jpg" /></li>
+</ul>
+     </div>
+        </div>
+        <div id="content">
+            <div id="r100">
+            </div>
+            <div id="r200"></div>
+            <div id="r300"></div>
+           
+        </div>
+        <div id="feedback">
+        </div>
+        <div id="dostavka">
+        </div><script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+       <script src="js/jquery.bxslider.js"></script> 
+        <script>
+        $(document).ready(function(){
+  $('.bxslider').bxSlider();
+});
+        </script>
+        
+    </body>
+    
+    
+</html>
